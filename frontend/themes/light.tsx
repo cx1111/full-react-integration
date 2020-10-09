ed28108999afcfaa6fd48351c906af4a1f4e0289
AJ Alon <aj.alon@datarobot.com>
2020-10-08 17:22:50 -0700

    consolidate defaultTheme into lightTheme

--- frontend/themes/light.tsx
+++ frontend/themes/light.tsx
@@ -3,6 +3,18 @@ import { createMuiTheme } from "@material-ui/core/styles";
 export const lightTheme = createMuiTheme({
   palette: {
     type: "light",
+    primary: {
+      main: "#556cd6",
+    },
+    secondary: {
+      main: "#19857b",
+    },
+    error: {
+      main: "#a400",
+    },
+    background: {
+      default: "#fff",
+    },
   },
 });
 export default lightTheme;
--- frontend/themes/themes.tsx
+++ frontend/themes/themes.tsx
@@ -2,14 +2,12 @@ import { Theme } from "@material-ui/core";
 
 import darkTheme from "./dark";
 import lightTheme from "./light";
-import defaultTheme from "./default";
 
 export default function getThemeByName(theme: string): Theme {
   return themeMap[theme];
 }
 
 const themeMap: { [key: string]: Theme } = {
-  defaultTheme,
   darkTheme,
   lightTheme,
 };
