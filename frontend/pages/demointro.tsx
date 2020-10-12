b544708a687ca5c300babeaaf5be5647ac7e33b4
AJ Alon <aj.alon@datarobot.com>
2020-10-12 11:43:14 -0700

    fix posts22 bug

--- frontend/pages/demointro.tsx
+++ frontend/pages/demointro.tsx
@@ -2,7 +2,7 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/demolayout";
 import utilStyles from "../styles/utils.module.css";
-import { getSortedPostsData } from "../lib/posts22";
+import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
 import Date from "../components/date";
 import { GetStaticProps } from "next";
