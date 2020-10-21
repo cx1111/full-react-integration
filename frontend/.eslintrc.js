76723dd7fe6067dfcb19f109595691482fb73210
Chen Xie <is.chenx@gmail.com>
2020-10-21 16:24:18 -0700

    Add demo usage for auth context

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -19,8 +19,8 @@ module.exports = {
     "no-console": ["warn", { allow: ["info", "warn", "error"] }],
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/camelcase": "off",
-    "@typescript-eslint/ban-ts-ignore": "off",
     "@typescript-eslint/member-delimiter-style": "off",
+    "@typescript-eslint/no-empty-function": "off",
     "@typescript-eslint/no-inferrable-types": "off",
     "@typescript-eslint/no-unused-vars": [
       "error",
