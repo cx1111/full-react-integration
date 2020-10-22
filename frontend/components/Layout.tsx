9fa982a161ee92422c3cb687e1f29964c2c8513f
Chen Xie <is.chenx@gmail.com>
2020-10-21 22:46:21 -0700

    Add navbar component and refactor layout

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,42 +1,19 @@
 // The base layout template for the site
 import React, { FunctionComponent } from "react";
 import Head from "next/head";
-import Link from "next/link";
 import styled, { ThemeProvider } from "styled-components";
 import { AuthProvider } from "../context/AuthContext";
 import {
   ThemeProvider as ThemeModeProvider,
   ThemeContext,
 } from "../context/ThemeContext";
-import {
-  AppBar,
-  IconButton,
-  Toolbar,
-  Typography,
-  Button,
-  makeStyles,
-  ThemeProvider as MuiThemeProvider,
-  NoSsr,
-} from "@material-ui/core";
-import SwitchUI from "@material-ui/core/Switch";
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import FormControlLabel from "@material-ui/core/FormControlLabel/FormControlLabel";
-import MenuIcon from "@material-ui/icons/Menu";
 import getThemeByName from "../themes/themes";
+import NavBar from "./Navbar";
 
 // Spacer for the fixed navbar
 const PlaceholderDiv = styled.div({ marginBottom: "80px" });
-
-const useStyles = makeStyles((_theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
-
-const ThemeSwitcher = styled(FormControlLabel)``;
 
 type LayoutProps = {
   title?: string;
@@ -46,8 +23,6 @@ const Layout: FunctionComponent<LayoutProps> = ({
   title = "React Django App",
   children,
 }) => {
-  const classes = useStyles();
-
   return (
     <>
       {/* Specify HTML head fields here */}
@@ -64,41 +39,12 @@ const Layout: FunctionComponent<LayoutProps> = ({
       <AuthProvider>
         <ThemeModeProvider>
           <ThemeContext.Consumer>
-            {({ themeName, toggleThemeName }) => (
+            {({ themeName }) => (
               <MuiThemeProvider theme={getThemeByName(themeName)}>
                 <ThemeProvider theme={getThemeByName(themeName)}>
+                  {/* TODO: remove? */}
                   <CssBaseline />
-                  {/* TODO Pull the AppBar into its own component file */}
-                  <NoSsr>
-                    <AppBar position="fixed" className={classes.root}>
-                      <Toolbar>
-                        <IconButton
-                          edge="start"
-                          color="inherit"
-                          aria-label="menu"
-                        >
-                          <MenuIcon />
-                        </IconButton>
-                        <Link href={"/"}>
-                          <Typography variant="h6" className={classes.title}>
-                            React Django
-                          </Typography>
-                        </Link>
-                        <ThemeSwitcher
-                          control={
-                            <SwitchUI
-                              checked={themeName === "dark"}
-                              onChange={toggleThemeName}
-                            />
-                          }
-                          label="Light Mode"
-                        />
-                        <Link href={"/login"}>
-                          <Button color="inherit">Login</Button>
-                        </Link>
-                      </Toolbar>
-                    </AppBar>
-                  </NoSsr>
+                  <NavBar />
                   <PlaceholderDiv />
                   <>{children}</>
                 </ThemeProvider>
---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,65 @@
+// The base layout template for the site
+import React, { FC } from "react";
+import Link from "next/link";
+// import { AuthProvider } from "../context/AuthContext";
+import { ThemeContext } from "../context/ThemeContext";
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Typography,
+  Button,
+  makeStyles,
+  NoSsr,
+} from "@material-ui/core";
+import SwitchUI from "@material-ui/core/Switch";
+import FormControlLabel from "@material-ui/core/FormControlLabel/FormControlLabel";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles((_theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const NavBar: FC = () => {
+  const classes = useStyles();
+
+  return (
+    <NoSsr>
+      <ThemeContext.Consumer>
+        {({ themeName, toggleThemeName }) => (
+          <AppBar position="fixed" className={classes.root}>
+            <Toolbar>
+              <IconButton edge="start" color="inherit" aria-label="menu">
+                <MenuIcon />
+              </IconButton>
+              <Link href={"/"}>
+                <Typography variant="h6" className={classes.title}>
+                  Full React Integration
+                </Typography>
+              </Link>
+              <FormControlLabel
+                control={
+                  <SwitchUI
+                    checked={themeName === "light"}
+                    onChange={toggleThemeName}
+                  />
+                }
+                label="Light Mode"
+              />
+              <Link href={"/login"}>
+                <Button color="inherit">Login</Button>
+              </Link>
+            </Toolbar>
+          </AppBar>
+        )}
+      </ThemeContext.Consumer>
+    </NoSsr>
+  );
+};
+
+export default NavBar;
