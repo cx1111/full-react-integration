d1901339b9136ff7b3a9b40853b3655e17138018
Chen Xie <is.chenx@gmail.com>
2020-11-03 23:15:02 -0800

    Create comment bare works

--- frontend/pages/posts/[postID].tsx
+++ frontend/pages/posts/[postID].tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Container, Typography } from "@material-ui/core";
+// import Divider from "@material-ui/core/Divider";
 import Card from "@material-ui/core/Card";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
 import { useRouter } from "next/router";
 import Layout from "../../components/Layout";
@@ -11,11 +14,14 @@ import {
   forumAPI,
   ViewPostResponse,
   ListPostCommentsResponse,
+  CreateCommentResponse,
+  CreateCommentError,
 } from "../../lib/endpoints/forum";
-import { parseError } from "../../lib/endpoints/error";
+import { parseError, isDefaultError } from "../../lib/endpoints/error";
 import { displayDate } from "../../lib/utils/date";
+import { AuthContext } from "../../context/AuthContext";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
     minWidth: 275,
   },
@@ -30,10 +36,23 @@ const useStyles = makeStyles({
   pos: {
     marginBottom: 12,
   },
-});
+  commentForm: {
+    marginTop: theme.spacing(1),
+    // display: "flex",
+    // flexDirection: "column",
+    // alignItems: "center",
+  },
+  form: {
+    display: "flex",
+    flexDirection: "column",
+    marginTop: theme.spacing(1),
+    alignItems: "left",
+  },
+}));
 
 const Posts: React.FC = ({}) => {
   const router = useRouter();
+  const { accessToken } = React.useContext(AuthContext);
   const postID =
     typeof router.query.postID === "string" ? router.query.postID : "";
   const classes = useStyles();
@@ -67,7 +86,10 @@ const Posts: React.FC = ({}) => {
         const response = await forumAPI.viewPost(postID);
         setPostData(response.data);
       } catch (e) {
-        setPostError(parseError(e));
+        const errorInfo = parseError<null>(e);
+        if (errorInfo) {
+          setPostError(errorInfo.detail);
+        }
         setPostData(undefined);
       } finally {
         setPostLoading(false);
@@ -76,24 +98,78 @@ const Posts: React.FC = ({}) => {
     getPosts();
   }, [postID, setPostData, setPostLoading, setPostError]);
 
-  React.useEffect(() => {
-    const getComments = async () => {
-      if (postID === "") {
-        return;
+  const loadComments = React.useCallback(async () => {
+    if (postID === "") {
+      return;
+    }
+    try {
+      setCommentsLoading(true);
+      const response = await forumAPI.listPostComments(postID);
+      setCommentsData(response.data);
+    } catch (e) {
+      const errorInfo = parseError<null>(e);
+      if (errorInfo) {
+        setCommentsError(errorInfo.detail);
       }
-      try {
-        setCommentsLoading(true);
-        const response = await forumAPI.listPostComments(postID);
-        setCommentsData(response.data);
-      } catch (e) {
-        setCommentsError(parseError(e));
-        setCommentsData(undefined);
-      } finally {
-        setCommentsLoading(false);
-      }
-    };
-    getComments();
+      setCommentsData(undefined);
+    } finally {
+      setCommentsLoading(false);
+    }
   }, [postID, setCommentsData, setCommentsLoading, setCommentsError]);
+
+  React.useEffect(() => {
+    loadComments();
+  }, [loadComments]);
+
+  const [newComment, setNewComment] = React.useState<string>("");
+
+  const [
+    {
+      // data: createCommentData,
+      error: createCommentError,
+      loading: createCommentLoading,
+    },
+    {
+      // setData: setCreateCommentData,
+      setError: setCreateCommentError,
+      setLoading: setCreateCommentLoading,
+    },
+  ] = useFetch<CreateCommentResponse, CreateCommentError>({ loading: false });
+
+  const postComment = async () => {
+    console.log("step 1", postID, accessToken);
+    if (postID === "" || !accessToken) {
+      return;
+    }
+    try {
+      console.log("trying");
+      setCreateCommentLoading(true);
+      setCreateCommentError(undefined);
+      const createCommentResponse = await forumAPI.createComment(
+        {
+          content: newComment,
+          post: postID,
+          is_reply: false,
+          parent_comment: null,
+        },
+        accessToken
+      );
+
+      // Some success alert
+      console.log(createCommentResponse.data);
+      loadComments();
+      alert("Your comment has been posted!");
+    } catch (e) {
+      const errorInfo = parseError<CreateCommentError>(e);
+      if (isDefaultError(errorInfo)) {
+        setCreateCommentError({ non_field_errors: [errorInfo.detail] });
+      } else {
+        setCreateCommentError(errorInfo);
+      }
+    } finally {
+      setCreateCommentLoading(false);
+    }
+  };
 
   if (postID === "") {
     return null;
@@ -115,6 +191,7 @@ const Posts: React.FC = ({}) => {
               {displayDate(postData.created_at)}
             </p>
             <Link href={"/posts"}>Back to Posts</Link>
+            <hr />
           </>
         )}
         {commentsLoading && <p>Loading comments...</p>}
@@ -137,6 +214,42 @@ const Posts: React.FC = ({}) => {
             ))}
           </>
         )}
+        <div className={classes.commentForm}>
+          {accessToken ? (
+            <form className={classes.form}>
+              <TextField
+                multiline
+                rows={6}
+                value={newComment}
+                onChange={(e) => setNewComment(e.target.value)}
+                required
+                variant="outlined"
+                margin="normal"
+              />
+              {createCommentError?.non_field_errors && (
+                <Typography color={"error"}>
+                  {createCommentError.non_field_errors[0]}
+                </Typography>
+              )}
+              <Button
+                // fullWidth={false}
+                type="submit"
+                variant="contained"
+                color="primary"
+                // className={classes.submit}
+                onClick={(e) => {
+                  e.preventDefault();
+                  postComment();
+                }}
+                disabled={createCommentLoading}
+              >
+                Post Comment
+              </Button>
+            </form>
+          ) : (
+            <>Log in to comment</>
+          )}
+        </div>
       </Container>
     </Layout>
   );
